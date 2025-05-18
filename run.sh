195273be16c65062a7648209b554110cc0c98d0d
ibuki2003 <main@fuwa.dev>
2025-05-19 02:13:57 +0900

    feat: run custom command from run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
-#!env bash
+#!/usr/bin/env bash
 
-TAG=${1:-v2024.1}
+cd $(dirname "$0")
+
+IMAGE=${IMAGE:-vivado:v2024.1-asz}
+
+CMD_DEFAULT='cd && . /opt/Xilinx/Vivado/2024.1/settings64.sh && _JAVA_AWT_WM_NONREPARENTING=1 LD_PRELOAD=/lib/x86_64-linux-gnu/libudev.so.1 vivado'
+CMD="${@:-$CMD_DEFAULT}"
 
 docker run \
   -it \
@@ -9,8 +14,8 @@ docker run \
   -e "REGID=$(id -g)" \
   -e DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-  -v ~/dev/vivado:/work \
   -v ./home:/home/vivado \
-  vivado:${TAG} \
-  /bin/bash -c 'cd && . /opt/Xilinx/Vivado/2024.1/settings64.sh && _JAVA_AWT_WM_NONREPARENTING=1 LD_PRELOAD=/lib/x86_64-linux-gnu/libudev.so.1 vivado'
+  -v ~/dev/vivado:/home/vivado/work \
+  "$IMAGE" \
+  /bin/bash -c "${CMD}"
 
