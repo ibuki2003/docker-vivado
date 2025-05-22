6edf83d9feaf086edebf9464fe37bb3027677d14
ibuki2003 <main@fuwa.dev>
2025-05-22 18:27:56 +0900

    feat: run.sh setup

--- run.sh
+++ run.sh
@@ -4,11 +4,7 @@ cd $(dirname "$0")
 
 IMAGE=${IMAGE:-vivado:v2024.1-asz}
 
-CMD=(
-  /bin/bash
-  -c
-  '. /opt/Xilinx/Vivado/2024.1/settings64.sh && _JAVA_AWT_WM_NONREPARENTING=1 LD_PRELOAD=/lib/x86_64-linux-gnu/libudev.so.1 vivado'
-)
+CMD=('vivado')
 
 if [ $# -gt 0 ]; then CMD=("$@"); fi
 
@@ -17,11 +13,13 @@ docker run \
   --rm \
   -e "REUID=$(id -u)" \
   -e "REGID=$(id -g)" \
+  -e "_JAVA_AWT_WM_NONREPARENTING=1" \
+  -e "LD_PRELOAD=/lib/x86_64-linux-gnu/libudev.so.1" \
   -e DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
   -v ./home:/home/vivado \
   -v ~/dev/vivado:/home/vivado/work \
   -w /home/vivado \
   "$IMAGE" \
-  "${CMD[@]}"
+  /bin/bash -c '. /opt/Xilinx/Vivado/2024.1/settings64.sh && "$@"' dummy "${CMD[@]}"
 
